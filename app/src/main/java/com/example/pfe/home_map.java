e2f6417d9a90a4866d498d12802b5eed0aa11552
ismailelghazi <ismail47el@gmail.com>
2022-05-07 18:08:24 +0000

    day6

--- app/src/main/java/com/example/pfe/home_map.java
+++ app/src/main/java/com/example/pfe/home_map.java
@@ -1,34 +1,68 @@
 package com.example.pfe;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class home_map extends AppCompatActivity {
+public class home_map extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
-    Button logout;
+    BottomNavigationView bottom_navigation;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_map);
-//        start();
-    }
-    public void start()
-    {
-        //logout = findViewById(R.id.loguot);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent main = new Intent( home_map.this,login.class);
-                startActivity(main);
 
-            }
-        });
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+
+        bottom_navigation.setOnNavigationItemSelectedListener(this);
+        //bottom_navigation.setSelectedItemId(R.id.navigation_map);
+        }
+    map firstFragment = new map();
+    nofication thirdFragment = new nofication();
+    search secondFragment  = new search();
+    profile fourtFragment = new profile();
+//    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        System.out.println("ana hna1");
+
+        switch (item.getItemId()) {
+            case R.id.navigation_map:
+                System.out.println("ana hna");
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
+                return true;
+
+            case R.id.navigation_search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, secondFragment).commit();
+                return true;
+
+            case R.id.navigation_notifications:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
+                return true;
+            case R.id.navigation_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fourtFragment).commit();
+                return true;
+        }
+        return false;
+
     }
+//    public void start()
+//    {
+//        //logout = findViewById(R.id.loguot);
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                Intent main = new Intent( home_map.this,login.class);
+//                startActivity(main);
+//
+//            }
+//        });
+//    }
 }
