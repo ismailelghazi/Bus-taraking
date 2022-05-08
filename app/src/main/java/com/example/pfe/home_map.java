d09b66cfe6d945e8702f75ab9498a4cc01b243f3
ismailelghazi <ismail47el@gmail.com>
2022-05-08 05:30:31 +0100

    day6

--- app/src/main/java/com/example/pfe/home_map.java
+++ app/src/main/java/com/example/pfe/home_map.java
@@ -3,66 +3,54 @@ package com.example.pfe;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class home_map extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+public class home_map extends AppCompatActivity {
 
-    BottomNavigationView bottom_navigation;
-
+    BottomNavigationView bottomNavigationView;
+    map firstFragment = new map();
+    nofication thirdFragment = new nofication();
+    search secondFragment  = new search();
+    profile fourtFragment = new profile();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_map);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,firstFragment).commit();
 
-        bottom_navigation = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_map:
+                        System.out.println("ana hna");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
+                        return true;
 
-        bottom_navigation.setOnNavigationItemSelectedListener(this);
-        //bottom_navigation.setSelectedItemId(R.id.navigation_map);
-        }
-    map firstFragment = new map();
-    nofication thirdFragment = new nofication();
-    search secondFragment  = new search();
-    profile fourtFragment = new profile();
-//    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        System.out.println("ana hna1");
+                    case R.id.navigation_search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, secondFragment).commit();
+                        return true;
 
-        switch (item.getItemId()) {
-            case R.id.navigation_map:
-                System.out.println("ana hna");
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
-                return true;
-
-            case R.id.navigation_search:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, secondFragment).commit();
-                return true;
-
-            case R.id.navigation_notifications:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
-                return true;
-            case R.id.navigation_profile:
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, fourtFragment).commit();
-                return true;
-        }
-        return false;
+                    case R.id.navigation_notifications:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
+                        return true;
+                    case R.id.navigation_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fourtFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
 
     }
-//    public void start()
-//    {
-//        //logout = findViewById(R.id.loguot);
-//        logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                FirebaseAuth.getInstance().signOut();
-//                Intent main = new Intent( home_map.this,login.class);
-//                startActivity(main);
-//
-//            }
-//        });
-//    }
+
+
+
+
+
 }
