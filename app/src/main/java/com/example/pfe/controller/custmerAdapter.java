f7a09c7a77a4ea33c9d48e276ea4ab4a41f05a47
FallenVas <elouahdizaki@gmail.com>
2022-06-14 14:09:15 +0100

    Day 12

--- app/src/main/java/com/example/pfe/controller/custmerAdapter.java
+++ app/src/main/java/com/example/pfe/controller/custmerAdapter.java
@@ -6,16 +6,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.pfe.R;
 import com.example.pfe.pages.search;
 
+import java.util.List;
+
 public class custmerAdapter extends BaseAdapter {
     Context context;
     String Listbus[];
-    int Listbusim[];
+    List<Integer> Listbusim;
+    List<String> busId;
+    List<String>  fromId;
+    List<String>  toId;
     LayoutInflater inflater;
-    public custmerAdapter(Context cnt, int[] busim){
+    public custmerAdapter(Context cnt, List<Integer> busim , List<String>  busId,
+                          List<String>  fromId,
+                          List<String>  toId){
+        this.busId = busId;
+        this.fromId = fromId;
+        this.toId= toId;
         this.context=cnt;
 //        this.Listbus= bus;
         this.Listbusim= busim;
@@ -27,7 +38,7 @@ public class custmerAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return Listbusim.length;
+        return Listbusim.size();
     }
 
     @Override
@@ -44,8 +55,14 @@ public class custmerAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         System.out.println(inflater);
         view= inflater.inflate(R.layout.activity_custemer,null);
+        TextView busIdTV = view.findViewById(R.id.busId);
+        TextView toIdTV = view.findViewById(R.id.toId);
+        TextView fromIdTV = view.findViewById(R.id.fromid);
+        fromIdTV.setText(fromId.get(i));
+        busIdTV.setText(busId.get(i));
+        toIdTV.setText(toId.get(i));
         ImageView Imagbus = view.findViewById(R.id.imagebus);
-        Imagbus.setImageResource(Listbusim[i]);
+        Imagbus.setImageResource(Listbusim.get(i));
         return view;
     }
 }
--- app/src/main/java/com/example/pfe/pages/search.java
+++ app/src/main/java/com/example/pfe/pages/search.java
@@ -1,6 +1,7 @@
 package com.example.pfe.pages;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +12,23 @@ import androidx.fragment.app.Fragment;
 
 import com.example.pfe.R;
 import com.example.pfe.controller.custmerAdapter;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class search extends Fragment {
-    String bus[]= {"bus1","bus2","bus3"};
+    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference mDbRef = mDatabase.getReference("Bus");
+
+    String bus[]= {"bus1","bus2","bus3","g"};
     int busim[]={R.drawable.bus_serch,R.drawable.bus_serch1,R.drawable.bus_serch2};
     ListView listView;
 //    // TODO: Rename parameter arguments, choose names that match
@@ -60,11 +74,42 @@ public class search extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_search, container, false);
-        listView = (ListView) v.findViewById(R.id.List);
-        custmerAdapter adapter = new custmerAdapter(getActivity(), busim);
-        listView.setAdapter(adapter);
+
+        mDbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> busId = new ArrayList<String>();
+                List<String> fromId= new ArrayList<String>();
+                List<String> toId= new ArrayList<String>();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    busId.add(ds.child("ID").getValue(String.class));
+                    fromId.add(ds.child("PlaceA").getValue(String.class));
+                    toId.add(ds.child("PlaceB").getValue(String.class));
+                    Log.d("TAG", busId + " / " + fromId);
+                }
+                List<Integer> images = new ArrayList<Integer>();
+                int j = 0;
+                for (int i = 0; i < busId.size(); i++) {
+                    images.add(busim[i]);
+                    j=j+1;
+                    if(j==3){
+                        j=0;
+                    }
+                }
+
+                listView = (ListView) v.findViewById(R.id.List);
+                custmerAdapter adapter = new custmerAdapter(getActivity(), images , busId , fromId , toId);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w("Failed to read value.", error.toException());
+            }
+        });
+        // Inflate the layout for this fragment
+
         return v;
 
     }
